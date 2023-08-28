244a20ce6f3adb38b82ac91a9e74fb6c77f00ae3
Bruno Pereira da Silva <bruno.psilva56@outlook.com>
2023-08-28 19:33:47 -0300

    git commit -m "Exercicio 7 modificado" git push

--- Lista_3/Exercicio7/Exercicio7.php
+++ Lista_3/Exercicio7/Exercicio7.php
@@ -1,40 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicio 7</title>
+    <title>Registro de Notas</title>
 </head>
 <body>
-<form action="Exercicio7_Resp.php" method="post">
-  <input type="text" name="nome" placeholder="Nome">
-  <input type="number" name="nota1" placeholder="Nota 1">
-  <input type="number" name="nota2" placeholder="Nota 2">
-  <input type="submit" value="Enviar">
-  <button type="button" onclick="adicionarAluno()">Adicionar aluno</button>
-</form>
-
-<script>
-function adicionarAluno() {
-  var inputNome = document.createElement("input");
-  inputNome.type = "text";
-  inputNome.name = "nome" + Math.random();
-  inputNome.placeholder = "Nome";
-
-  var inputNota1 = document.createElement("input");
-  inputNota1.type = "number";
-  inputNota1.name = "nota1" + Math.random();
-  inputNota1.placeholder = "Nota 1";
-
-  var inputNota2 = document.createElement("input");
-  inputNota2.type = "number";
-  inputNota2.name = "nota2" + Math.random();
-  inputNota2.placeholder = "Nota 2";
-
-  document.querySelector("form").appendChild(inputNome);
-  document.querySelector("form").appendChild(inputNota1);
-  document.querySelector("form").appendChild(inputNota2);
-}
-</script>
+    <h1>Registro de Notas</h1>
+    <form action="result.php" method="post">
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<h2>Aluno $i</h2>";
+            echo "<label>Nome: <input type='text' name='alunos[$i][nome]' required></label><br>";
+            echo "<label>Nota 1: <input type='number' name='alunos[$i][nota1]' step='0.01' required></label><br>";
+            echo "<label>Nota 2: <input type='number' name='alunos[$i][nota2]' step='0.01' required></label><br><br>";
+        }
+        ?>
+        <input type="submit" value="Calcular">
+    </form>
 </body>
 </html>
--- Lista_3/Exercicio7/Exercicio7_Resp.php
+++ Lista_3/Exercicio7/Exercicio7_Resp.php
@@ -1,54 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicio 7</title>
+    <title>Resultados</title>
 </head>
-
 <body>
+    <h1>Resultados</h1>
     <?php
+    $alunos = $_POST['alunos'];
+    
+    $aprovados = [];
+    $reprovados = [];
 
-    // Declara um array para armazenar os dados dos alunos
-    $alunos = [];
-
-    // Lê os dados enviados pelo formulário
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $numAlunos = count($_POST);
-
-        for ($i = 0; $i < $numAlunos; $i++) {
-            $alunos[] = [
-                "nome" => $_POST["nome" . $i],
-                "nota1" => intval($_POST["nota1" . $i]),
-                "nota2" => intval($_POST["nota2" . $i]),
-                "media" => ($alunos[$i]["nota1"] + $alunos[$i]["nota2"]) / 2
-            ];
-        }
-    }
-
-    // Ordena os alunos pela média
-    usort($alunos, function ($a, $b) {
-        return $a["media"] > $b["media"];
-    });
-
-    // Imprime os dados dos alunos
-    echo "<h1>Aprovados</h1>";
     foreach ($alunos as $aluno) {
-        if ($aluno["media"] >= 7) {
-            echo "<p>Nome: " . $aluno["nome"] . " - Média: " . $aluno["media"] . "</p>";
+        $nome = $aluno['nome'];
+        $nota1 = floatval($aluno['nota1']);
+        $nota2 = floatval($aluno['nota2']);
+        $media = ($nota1 + $nota2) / 2;
+        
+        if ($media >= 7) {
+            $aprovados[$nome] = $media;
+        } else {
+            $reprovados[] = $nome;
         }
     }
 
-    echo "<h1>Reprovados</h1>";
-    foreach ($alunos as $aluno) {
-        if ($aluno["media"] < 7) {
-            echo "<p>Nome: " . $aluno["nome"] . " - Média: " . $aluno["media"] . "</p>";
-        }
+    echo "<h2>Aprovados:</h2>";
+    foreach ($aprovados as $nome => $media) {
+        echo "$nome - Média: $media<br>";
     }
 
+    echo "<h2>Reprovados:</h2>";
+    foreach ($reprovados as $nome) {
+        echo "$nome<br>";
+    }
     ?>
-
 </body>
-
 </html>
